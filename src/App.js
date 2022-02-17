3bd58d13e4ae821b2d428b74e0667fe9f644751e
RitvikSardana <65544983+RitvikSardana@users.noreply.github.com>
2022-02-17 19:43:01 +0530

    Update App.js
    Added Comments

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     const getMyNFTs = async () =>{
       const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x02dBc8D4A835670566aa7CD892f4B8B337357C5e&order_direction=asc')
       setGoalsData(openSeaData.data.assets)
-      // console.log(openSeaData.data.assets)
+      // console.log(openSeaData.data.assets)akakakaka
     }
     return getMyNFTs()
   },[])
